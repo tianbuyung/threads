4c95bf7a212fba3be3664a68475903051503cbb8
Septian Maulana <septianm028@gmail.com>
2023-08-28 20:47:59 +0700

    feat: create onboarding structure component

---
+++ components/forms/AccountProfile.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
+
+function AccountProfile({ user, btnTitle }: Props) {
+  return <div>AccountProfile</div>;
+}
+
+export default AccountProfile;
