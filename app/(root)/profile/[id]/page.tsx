4b0b61c9a44b7c09fbd79e11f0148aa54f0c11e6
Septian Maulana <septianm028@gmail.com>
2023-09-07 22:10:50 +0700

    feat: create header in profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,5 +1,28 @@
-function Page() {
-  return <div>page</div>;
-}
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { fetchUser } from "@/lib/actions/user.actions";
+import ProfileHeader from "@/components/shared/ProfileHeader";
+
+const Page = async ({ params }: { params: { id: string } }) => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(params.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  return (
+    <section>
+      <ProfileHeader
+        accountId={userInfo.id}
+        authUserId={user.id}
+        name={userInfo.name}
+        username={userInfo.username}
+        imgUrl={userInfo.image}
+        bio={userInfo.bio}
+      />
+    </section>
+  );
+};
 
 export default Page;
